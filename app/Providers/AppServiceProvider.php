80229c079128ee1bdd10bad3804e161d7dbe6669
CPOSEMAN-LAP <zranmomineseverin@gmail.com>
2026-08-19 10:31:51 +0000

    fix: keep Swagger routes synchronized

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        config()->set('l5-swagger.defaults.generate_always', true);
     }
 
     /**
